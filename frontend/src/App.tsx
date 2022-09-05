2654708f335b051a436adee18b8fc051dbfeb9ea
Messias Neto <messiascastro@gmail.com>
2022-09-05 20:05:29 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import NoticationButton from "./components/NotificationButton"
+
 function App() {
-  return(<h1>Olá mundo!</h1>)
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NoticationButton />
+    </>
+  )
 }
 
 export default App
